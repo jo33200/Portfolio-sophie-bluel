f278507412a4ae502b5c32d036a315a87d22f21f
jo33200 <jo33200@gmail.com>
2024-01-18 18:33:41 +0100

    fix: login js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,66 +1,54 @@
-//------------------------Ajout de l'évènement
+/*  définition des éléments du formulaire
+ définir Email, mot de passe et bouton d'envoi*/
 
-document.addEventListener("DOMContentLoaded", function () {
-    const loginButton = document.getElementById("loginButton");
-    loginButton.addEventListener("click", login);
-});
+ /*ajouter un écouteur d'événement à l'envoi du formulaire
+ quand le formulaire de connection est fourni, on va executer la fonction suivante
+ empecher l'envoi par défaut et on lance la verification du formulaire*/
+
+
+ /*verification de l'identifiant si c'est un email
+ --si l'email ne correspond pas, affichage message d'erreur et arret de l'envoi
+ --si l'email est valide, on envoi requete POST à l'adresse de l'API avec à l'interieur:
+ .en-tête: content type application JSON
+ .body de la requete du json qui contient l'email et le mot de passe
+ --attendre la réponse et convertir la réponse en JSON
+ --stocker le token dans le stockage local => rediriger vers l'index.html
+ --si pas de token message d'erreur de connection*/
 
 
 
-//-----------------------fonctions d'authentifications
+ const password = document.querySelector("#password")
+ const email = document.querySelector("#email")
+ const loginButton = document.querySelector("#loginButton")
 
-// récupération et stockage du token
-function saveToken(token) {
-    localStorage.setItem("userToken", token);
-}
-
-// fonction d'authentification
-function login( event) {
-    event.preventDefault();
-    
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    // Construire les données à envoyer avec la requête POST
-    const data = {
-        email: email,
-        password: password
-    };
-
-    // Effectuer la requête vers l'API
-    fetch("http://localhost:5678/api/users/login", {
+ loginButton.addEventListener("submit",async (event) => {
+    event.preventDefault()
+    const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+    if (emailRegex.test(email.value)===false){
+        const messageError=document.querySelector(".erreurEmail")
+        messageError.style.display="flex"
+        return
+    } else{
+        fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
+            //"autorisation": token
         },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (!response.ok) {
-            return response.json();
-        } else{ 
-            throw new Error(`Erreur de connexion : ${response.status}`);
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    }).then(response=>response.json())
+    .then(data=>{
+        let token=data.token
+        if (token){
+            localStorage.setItem("Token", token)
+            window.location.replace("index.html")
+        }else{
+            const erreurPassword=document.querySelector(".erreurMotdepasse")
+            erreurPassword.style.display="flex"
         }
     })
-    .then(data => {
-        // Traiter la réponse de l'API après une connexion réussie
-        document.getElementById("loginMessage").textContent = "authentification réussi!";
-        console.log(data); // Vous pouvez gérer la réponse ici
-
-        // Récupérer le token de connexion depuis la réponse de l'API
-        const userData = data.token;
-
-        // sauvegarder le token
-        saveToken(userData);
-
-        // redirection vers la page index.html
-        document.location.href = "index.html";
-
-        
-    })
-    .catch(error => {
-        // Traiter les erreurs lors de la connexion
-        document.getElementById("loginMessage").textContent = "Erreur d'email ou de mot de passe";
-        console.error("Erreur lors de la connexion :", error);
-    });
-}
+    }
+ })
