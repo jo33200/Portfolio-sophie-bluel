9a2e5a9f15347700637c32e92357b5d78a59908b
jo33200 <jo33200@gmail.com>
2024-01-19 14:47:36 +0100

    fix: comment login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,34 +1,33 @@
 /*  définition des éléments du formulaire
  définir Email, mot de passe et bouton d'envoi*/
 
- /*ajouter un écouteur d'événement à l'envoi du formulaire
- quand le formulaire de connection est fourni, on va executer la fonction suivante
- empecher l'envoi par défaut et on lance la verification du formulaire*/
-
-
- /*verification de l'identifiant si c'est un email
- --si l'email ne correspond pas, affichage message d'erreur et arret de l'envoi
- --si l'email est valide, on envoi requete POST à l'adresse de l'API avec à l'interieur:
- .en-tête: content type application JSON
- .body de la requete du json qui contient l'email et le mot de passe
- --attendre la réponse et convertir la réponse en JSON
- --stocker le token dans le stockage local => rediriger vers l'index.html
- --si pas de token message d'erreur de connection*/
-
-
-
  const password = document.querySelector("#password")
  const email = document.querySelector("#email")
  const loginButton = document.querySelector("#loginButton")
 
+
+/*ajouter un écouteur d'événement à l'envoi du formulaire
+ quand le formulaire de connection est fourni, on va executer la fonction suivante
+ empecher l'envoi par défaut et on lance la verification du formulaire*/
+
  loginButton.addEventListener("submit",async (event) => {
+
+    // bloquer le comportement du navigateur par défaut
     event.preventDefault()
+
+    // variable pour la regex (verification si c'est un email)
     const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+
+    // regex de l'email n'est pas bonne
     if (emailRegex.test(email.value)===false){
+        // apparition du message d'erreur
         const messageError=document.querySelector(".erreurEmail")
         messageError.style.display="flex"
-        return
+        return// arret de l'envoi
     } else{
+        /*si l'email est valide, on envoi requete POST à l'adresse de l'API avec à l'interieur:
+        .en-tête: content type application JSON
+        .body de la requete du json qui contient l'email et le mot de passe*/
         fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
@@ -39,13 +38,14 @@
             email: email.value,
             password: password.value
         })
-    }).then(response=>response.json())
+    }).then(response=>response.json())//attendre la réponse et convertir la réponse en JSON
     .then(data=>{
+        //stocker le token dans le stockage local => rediriger vers l'index.html
         let token=data.token
         if (token){
             localStorage.setItem("Token", token)
             window.location.replace("index.html")
-        }else{
+        }else{ //si pas de token message d'erreur de connection
             const erreurPassword=document.querySelector(".erreurMotdepasse")
             erreurPassword.style.display="flex"
         }
