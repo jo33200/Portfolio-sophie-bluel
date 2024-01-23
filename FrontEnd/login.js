4ffd8d36172ce244459c0471f7f699892fac9dd3
jo33200 <jo33200@gmail.com>
2024-01-23 11:51:10 +0100

    fix: resolution of redirection bug and error message

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,17 +6,22 @@
  const loginButton = document.querySelector("#loginButton")
 
 
+
 /*ajouter un écouteur d'événement à l'envoi du formulaire
  quand le formulaire de connection est fourni, on va executer la fonction suivante
  empecher l'envoi par défaut et on lance la verification du formulaire*/
 
- loginButton.addEventListener("submit",async (event) => {
+ document.querySelector("form").addEventListener("submit",async (event) => {
 
     // bloquer le comportement du navigateur par défaut
     event.preventDefault()
-
+    
     // variable pour la regex (verification si c'est un email)
     const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+
+    // Réinitialiser l'affichage des messages d'erreur
+    document.querySelector(".erreurEmail").style.display = "none";
+    document.querySelector(".erreurMotdepasse").style.display = "none";
 
     // regex de l'email n'est pas bonne
     if (emailRegex.test(email.value)===false){
@@ -38,8 +43,8 @@
             email: email.value,
             password: password.value
         })
-    }).then(response=>response.json())//attendre la réponse et convertir la réponse en JSON
-    .then(data=>{
+        }).then(response=>response.json())//attendre la réponse et convertir la réponse en JSON
+        .then(data=>{
         //stocker le token dans le stockage local => rediriger vers l'index.html
         let token=data.token
         if (token){
@@ -52,3 +57,18 @@
     })
     }
  })
+
+ /* Ajouter un deuxième écouteur d'événements au clic sur le bouton de connexion
+ Cela gère spécifiquement les erreurs d'email incorrectes sans empêcher la soumission du formulaire */
+
+loginButton.addEventListener("click", () => {
+    // Variable pour la regex (vérification si c'est un email)
+    const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+
+    // Si la regex de l'email n'est pas correcte
+    if (emailRegex.test(email.value) === false) {
+        // Affichage du message d'erreur
+        const messageError = document.querySelector(".erreurEmail");
+        messageError.style.display = "flex";
+    }
+});
